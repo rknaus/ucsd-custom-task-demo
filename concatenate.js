7ab6831e1adf9ed0000cb1660d2c719ee402d8f3
Knaus <knaus@netcloud.ch>
2017-07-19 17:54:08 +0200

    fix concatenation and function call

--- concatenate.js
+++ concatenate.js
@@ -1,9 +1,9 @@
 var input1 = input.input1;
 var input2 = input.input2;
 
-output.output1 = concatenate();
+output.output1 = concatenate(input1, input2);
 
 function concatenate(input1, input2) {
-  var concatenated = input1.toString() + input2.toString();
+  var concatenated = input1 + input2;
   return concatenated;
 }
